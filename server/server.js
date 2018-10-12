a0fe7772914c0cfee161009890e2e4a601fbcec7
atrivedi1 <akash.trivedi1@gmail.com>
2018-10-11 19:35:30 -0700

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -18,14 +18,32 @@ app.use(bodyParser.json());
 io.on('connection', (socket) => {
 	console.log("New user connected");
 
+	socket.emit('newMessage', {
+		from: 'Admin',
+		text: 'Welcome to the coolest chat app on the planet!',
+		createdAt: new Date().getTime()
+	});
+
+	socket.broadcast.emit('newMessage', {
+		from: 'Admin',
+		text: 'New user joined!',
+		createdAt: new Date().getTime()
+	})
+	
 	socket.on('createMessage', (message) => {
 		console.log('Message created:', message);
-		
+
 		io.emit('newMessage', {
 			from: message.from,
 			text: message.text,
 			createdAt: new Date().getTime()
 		})
+		
+		// socket.broadcast.emit('newMessage', {
+		// 	from: message.from,
+		// 	text: message.text,
+		// 	createdAt: new Date().getTime()
+		// })
 	})
 	
 	socket.on('disconnect', () => {
